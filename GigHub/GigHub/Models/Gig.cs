b61f4f60cc7edd6834656d499b5f4138ee4c80e2
Sukh Dol <ssdol@outlook.com>
2016-06-15 09:15:14 -0600

    Implement saving a gig.

--- GigHub/GigHub/Models/Gig.cs
+++ GigHub/GigHub/Models/Gig.cs
@@ -8,15 +8,18 @@ namespace GigHub.Models
         public int Id { get; set; }
 
         [Required]
-        public ApplicationUser Artist { get; set; }
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
         [Required]
         [StringLength(255)]
         public string Venue { get; set; }
-        
+
         [Required]
+        public byte GenreId { get; set; }
+
+        public ApplicationUser Artist { get; set; }
         public Genre Genre { get; set; }
     }
 }
