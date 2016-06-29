73531c6ddcb3eb9e0a0d62e6a27c3e6106563fdf
Sukh Dol <ssdol@outlook.com>
2016-06-28 19:57:23 -0600

    Implement ReceiveNotifications.

---
+++ GigHub/GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<ApplicationUser, UserDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+            Mapper.CreateMap<Gig, GigDto>();
+            Mapper.CreateMap<Notification, NotificationDto>();
+        }
+
+        protected override void Configure()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ApplicationUser, UserDto>();
+                cfg.CreateMap<Genre, GenreDto>();
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
+            });
+        }
+    }
+}
